6538177acdede1eafb84f28b08941ad198cd7520
Nish Dahanayake <nishdahanayake@rebs-air.marcellin.internal>
2016-08-02 11:45:46 +1000

    initial commit

---
+++ www/js/app.js
@@ -0,0 +1,19 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+.controller('ListCtrl', function($scope){
+    $scope.items = [];
+    $scope.addItem = function() {
+        var name = prompt('What do you need to buy?');
+        if (name) {
+            $scope.items.push({
+                'name':name
+                              
+                              });
+            }
+      };
+});
